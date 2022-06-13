30dcb746cd09caf54ff53f8b93f35fa570c352a8
Dheeraj <gandhidheeraj9@gmail.com>
2022-06-13 12:03:37 +0530

    chat integrated

--- client/src/Components/Roomcard/Roomcard.jsx
+++ client/src/Components/Roomcard/Roomcard.jsx
@@ -10,13 +10,15 @@ import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import { red } from "@mui/material/colors";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
+
 import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
 import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+import { Backdrop, Button } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import PostChat from "../../Components/Chat/PostChat";
+import { ChatState } from "../../Context/Provider";
 import Grid from "@mui/material/Grid";
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
@@ -29,74 +31,139 @@ const ExpandMore = styled((props) => {
   }),
 }));
 
-export default function RecipeReviewCard() {
+export default function Roomcard() {
   const [expanded, setExpanded] = React.useState(false);
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
+  const { setSelectedChat, user, chats, setChats } = ChatState();
+
+  const [showChat, setShowChat] = useState(false);
+
+  const fetchChats = async () => {
+    //console.log(user._id);
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+
+      const { data } = await axios.get("/api/chat", config);
+      setChats(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const accessChat = async (userId) => {
+    console.log(chats);
+
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.post(`/api/chat`, { userId }, config);
+      console.log(data);
+
+      if (!chats.find((c) => c._id === data._id)) {
+        setChats([data, ...chats]);
+      }
+      setSelectedChat(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchChats();
+  }, [fetchChats]);
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <Grid container justifyContent="space-between" alignItems="center">
-        <Grid item xs={8}>
-          <CardHeader
-            avatar={
-              <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                R
-              </Avatar>
-            }
-            title="Dheeraj Gandhi"
-          />
-        </Grid>
-        <Grid item xs={4}>
-          <Typography
-            sx={{
-              bgcolor: "#6177D4",
-              color: "white",
-              padding: ".5rem 1rem",
-              borderRadius: ".1rem",
-            }}
-            variant="h7"
+    <>
+      {showChat ? (
+        <div>
+          <Backdrop
+            sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+            open={showChat}
           >
-            $2393
-          </Typography>
+            <PostChat setShowChat={setShowChat} />
+          </Backdrop>
+        </div>
+      ) : (
+        <></>
+      )}
+      <Card sx={{ maxWidth: 345 }}>
+        <Grid container justifyContent="space-between" alignItems="center">
+          <Grid item xs={8}>
+            <CardHeader
+              avatar={
+                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                  R
+                </Avatar>
+              }
+              title="Dheeraj Gandhi"
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <Typography
+              sx={{
+                bgcolor: "#6177D4",
+                color: "white",
+                padding: ".5rem 1rem",
+                borderRadius: ".1rem",
+              }}
+              variant="h7"
+            >
+              $2393
+            </Typography>
+          </Grid>
         </Grid>
-      </Grid>
 
-      <CardMedia
-        component="img"
-        height="194"
-        image="/static/images/cards/paella.jpg"
-        alt="Paella dish"
-      />
-      <CardContent>
-        <Typography variant="h7" color="text.primary" fontWeight="bolder">
-          Spacious rooms for rent with no deposit
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          5316 Tinker St, Boise, Illinois, United States
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Grid container align="center">
-          <Grid item xs={4}>
-            <IconButton aria-label="add to favorites">
-              <ChatBubbleOutlineIcon sx={{ fontSize: "25px" }} />
-            </IconButton>
+        <CardMedia
+          component="img"
+          height="194"
+          image="/static/images/cards/paella.jpg"
+          alt="Paella dish"
+        />
+        <CardContent>
+          <Typography variant="h7" color="text.primary" fontWeight="bolder">
+            Spacious rooms for rent with no deposit
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            5316 Tinker St, Boise, Illinois, United States
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Grid container align="center">
+            <Grid item xs={4}>
+              <IconButton
+                onClick={() => {
+                  setShowChat(true);
+                  accessChat("628273c939b9dd3b346ec13a");
+                }}
+                aria-label="add to favorites"
+              >
+                <ChatBubbleOutlineIcon sx={{ fontSize: "25px" }} />
+              </IconButton>
+            </Grid>
+            <Grid item xs={4}>
+              <IconButton aria-label="share">
+                <StarBorderOutlinedIcon sx={{ fontSize: "28px" }} />
+              </IconButton>
+            </Grid>
+            <Grid item xs={4}>
+              <IconButton aria-label="location">
+                <LocationOnOutlinedIcon sx={{ fontSize: "28px" }} />
+              </IconButton>
+            </Grid>
           </Grid>
-          <Grid item xs={4}>
-            <IconButton aria-label="share">
-              <StarBorderOutlinedIcon sx={{ fontSize: "28px" }} />
-            </IconButton>
-          </Grid>
-          <Grid item xs={4}>
-            <IconButton aria-label="location">
-              <LocationOnOutlinedIcon sx={{ fontSize: "28px" }} />
-            </IconButton>
-          </Grid>
-        </Grid>
-      </CardActions>
-    </Card>
+        </CardActions>
+      </Card>
+    </>
   );
 }
--- client/src/Pages/Roompage.jsx
+++ client/src/Pages/Roompage.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import Roomcard from "../Components/Roomcard/Roomcard.jsx";
 import Navbar from "../Components/Navbar/Navbar.js";
+import { Grid } from "@material-ui/core";
 const Roompage = () => {
   return (
     <>
       <Navbar></Navbar>
-      <Roomcard></Roomcard>
-      <Roomcard></Roomcard>
-      <Roomcard></Roomcard>
-      <Roomcard></Roomcard>
-      <Roomcard></Roomcard>
-      <Roomcard></Roomcard>
+      <Grid container>
+        <Grid item xs={4}>
+          <Roomcard></Roomcard>
+        </Grid>
+        <Grid item xs={4}>
+          <Roomcard></Roomcard>
+        </Grid>
+      </Grid>
     </>
   );
 };
