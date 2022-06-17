974deff7dc2f79fccb697eb0810227d0ecc0056e
Dheeraj <gandhidheeraj9@gmail.com>
2022-06-17 11:06:18 +0530

    bug fix

--- client/src/Services/index.js
+++ client/src/Services/index.js
@@ -4,4 +4,6 @@ export const signUp = (params) => axios.post("/signup", params);
 export const postRoom = (params, token) => axios.post("/rooms", params, token);
 export const Login = (params) => axios.post("/login", params);
 export const getallPosts = (params) => axios.get("/rooms", params);
+export const postroommate = (params, token) =>
+  axios.post("/roommateprofiles", params, token);
 // export const deletePost = (params) => axios.post("/deleteRoom", params);
--- client/src/State/Actions/postroomAction.js
+++ client/src/State/Actions/postroomAction.js
@@ -1,4 +1,5 @@
 import { postRoom } from "../../Services";
+import { postroommate } from "../../Services";
 export const postRoomAction = (params) => (dispatch) => {
   const usertoken = JSON.parse(localStorage.getItem("token"));
   console.log(usertoken);
@@ -19,6 +20,31 @@ export const postRoomAction = (params) => (dispatch) => {
     .catch((err) =>
       dispatch({
         type: "POSTROOMFAIL",
+        payload: err.response.data,
+      })
+    );
+};
+
+export const postRoommateAction = (params) => (dispatch) => {
+  const usertoken = JSON.parse(localStorage.getItem("token"));
+  console.log(usertoken);
+  const config = {
+    headers: {
+      Authorization: `Bearer ${usertoken}`,
+      "Content-type": "application/json",
+    },
+  };
+  postroommate(params, config)
+    .then((res) => {
+      console.log(res.data);
+      dispatch({
+        type: "POSTROOMMATE",
+        payload: res.data,
+      });
+    })
+    .catch((err) =>
+      dispatch({
+        type: "POSTROOMMATEFAIL",
         payload: err.response.data,
       })
     );
