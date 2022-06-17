252941ca145197a8539b761b3dfa6d9864794f51
ParthVernekar <parthoffspinner@gmail.com>
2022-06-17 10:46:23 +0530

    removed and uninstalled unused dependencies

--- backend/cloudinary/index.js
+++ backend/cloudinary/index.js
@@ -1,5 +1,4 @@
 const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
