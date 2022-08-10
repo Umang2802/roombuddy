8384e9a19a84505f58671d31372c8652bcc10775
Umang2802 <umangmetri@gmail.com>
2022-08-10 21:18:48 +0530

    added .env to backend

--- backend/index.js
+++ backend/index.js
@@ -44,7 +44,9 @@ app.use("/starredRoommates", starredRoommateRoutes);
 app.use("/chat", chatRoutes);
 app.use("/message", messageRoutes);
 
-const server = app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () => {
   console.log("Serving on port 5000");
 });
 
