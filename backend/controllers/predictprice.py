068c523412943873be564a574bd0a3e0b43dd07c
Umang2802 <umangmetri@gmail.com>
2022-08-14 00:08:35 +0530

    fixing controller/predictprice.py path not found error6

--- backend/controllers/predictprice.py
+++ backend/controllers/predictprice.py
@@ -1,7 +1,7 @@
 import pickle
 import pandas as pd
 import sys
-model = pickle.load(open('controllers/StackModel.pkl','rb'))    
+model = pickle.load(open('backend/controllers/StackModel.pkl','rb'))    
 input = pd.DataFrame([[sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4]]], columns=['location','total_sqft','bath','bhk'])
 print(model.predict(input))
 sys.stdout.flush()
