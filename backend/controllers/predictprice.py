bea49d990432288757a203e484c5da2b769dadaf
Umang2802 <umangmetri@gmail.com>
2022-08-20 02:41:47 +0530

    heroku hosting3

--- backend/controllers/predictprice.py
+++ backend/controllers/predictprice.py
@@ -3,8 +3,8 @@ import pandas as pd
 import numpy as np
 import sys
 model = pickle.load(open('backend/controllers/StackModel.pkl','rb'))    
-input = pd.DataFrame([[sys.argv[1], np.loglp(sys.argv[2]), np.loglp(sys.argv[3]),
-                     np.loglp(sys.argv[4])]], columns=['location', 'total_sqft', 'bath', 'bhk'])
+input = pd.DataFrame([[sys.argv[1], np.log1p(sys.argv[2]), np.log1p(sys.argv[3]),
+                     np.log1p(sys.argv[4])]], columns=['location', 'total_sqft', 'bath', 'bhk'])
 print(model.predict(input))
 sys.stdout.flush()
 
